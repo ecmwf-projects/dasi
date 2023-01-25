ba544ef23a50872f7c8ea137385b313588d48485
Metin Cakircali <metin.cakircali@ecmwf.int>
2023-01-25 15:17:47 +0100

    feat: start new c api
    Starting new C API to cleanup and port old API

---
+++ src/dasi/api/c/Dasi.cc
@@ -0,0 +1,198 @@
+/*
+ * (C) Copyright 2023 ECMWF.
+ * This software is licensed under the terms of the Apache Licence Version 2.0
+ * which can be obtained at http: //www.apache.org/licenses/LICENSE-2.0.
+ * In applying this licence, ECMWF does not waive the privileges and immunities
+ * granted to it by virtue of its status as an intergovernmental organisation
+ * nor does it submit to any jurisdiction.
+ */
+
+#include "dasi/api/c/Dasi.h"
+#include "dasi/api/Dasi.h"
+
+#include <eckit/exception/Exceptions.h>
+
+// -----------------------------------------------------------------------------
+//                           ERROR HANDLING
+// -----------------------------------------------------------------------------
+
+struct DasiError {
+    DasiError() : Code(DASI_SUCCESS) {}
+    void Set(const std::string& msg, const dasi_error_enum_t code) {
+        this->Code    = code;
+        this->Message = msg;
+    }
+    std::string Message;
+    dasi_error_enum_t Code;
+};
+
+const char* dasi_error_get_message(const dasi_error_t error) {
+    return error->Message.c_str();
+}
+
+dasi_error_enum_t dasi_error_get_code(const dasi_error_t error) {
+    return error->Code;
+}
+
+DasiError* g_error = new DasiError();
+
+/**
+ * @brief Catch the exceptions and capture the detail in dasi_error_t.
+ * @param error  Pointer to the error message and code
+ * @param fn     Function that throws an exception
+ * @return true  No exception caught
+ * @return false Exception caught
+ */
+template <typename F>
+bool tryCatch(dasi_error_t* error, F&& fn) {
+    *error = g_error;
+    try {
+        fn();
+        // eckit::Log::debug() << "[Success]" << std::endl;
+        g_error->Set("Success", DASI_SUCCESS);
+    }
+    catch (const eckit::UserError& e) {
+        eckit::Log::error() << "User Error: " << e.what() << std::endl;
+        g_error->Set(e.what(), DASI_ERROR_USER);
+        return false;
+    }
+    catch (const eckit::Exception& e) {
+        eckit::Log::error() << "DASI Error: " << e.what() << std::endl;
+        g_error->Set(e.what(), DASI_ERROR);
+        return false;
+    }
+    catch (const std::exception& e) {
+        eckit::Log::error() << "Unkown Error: " << e.what() << std::endl;
+        g_error->Set(e.what(), DASI_ERROR_UNKNOWN);
+        return false;
+    }
+    catch (...) {
+        eckit::Log::error() << "Unkown Error!" << std::endl;
+        g_error->Set("Unknown Error", DASI_ERROR_UNKNOWN);
+        return false;
+    }
+    return true;
+}
+
+// -----------------------------------------------------------------------------
+//                           SESSION
+// -----------------------------------------------------------------------------
+
+dasi_t dasi_new(const char* filename, dasi_error_t* error) {
+    dasi::Dasi* result = nullptr;
+    tryCatch(error, [filename, &result] {
+        ASSERT(filename != nullptr);
+        result = std::make_unique<dasi::Dasi>(filename).release();
+    });
+    return reinterpret_cast<dasi_t>(result);
+}
+
+void dasi_delete(dasi_t p_session, dasi_error_t* error) {
+    auto* session = reinterpret_cast<dasi::Dasi*>(p_session);
+    tryCatch(error, [session] {
+        ASSERT(session != nullptr);
+        delete session;
+    });
+}
+
+void dasi_archive(dasi_t p_session, dasi_key_t p_key, const void* data,
+                size_t length, dasi_error_t* error) {
+    auto* session = reinterpret_cast<dasi::Dasi*>(p_session);
+    auto* key     = reinterpret_cast<dasi::Key*>(p_key);
+    tryCatch(error, [session, key, data, length] {
+        ASSERT(session != nullptr);
+        ASSERT(key != nullptr);
+        ASSERT(data != nullptr);
+        session->archive(*key, data, length);
+    });
+}
+
+void dasi_flush(dasi_t p_session, dasi_error_t* error) {
+    auto* session = reinterpret_cast<dasi::Dasi*>(p_session);
+    tryCatch(error, [session] {
+        ASSERT(session != nullptr);
+        session->flush();
+    });
+}
+
+// -----------------------------------------------------------------------------
+//                           KEY
+// -----------------------------------------------------------------------------
+
+dasi_key_t dasi_key_new(dasi_error_t* error) {
+    dasi::Key* result = nullptr;
+    tryCatch(error,
+             [&result] { result = std::make_unique<dasi::Key>().release(); });
+    return reinterpret_cast<dasi_key_t>(result);
+    ;
+}
+
+void dasi_key_delete(dasi_key_t p_key, dasi_error_t* error) {
+    auto* key = reinterpret_cast<dasi::Key*>(p_key);
+    tryCatch(error, [key] {
+        ASSERT(key != nullptr);
+        delete key;
+    });
+}
+
+void dasi_key_set(dasi_key_t p_key, const char* keyword, const char* value,
+                  dasi_error_t* error) {
+    auto* key = reinterpret_cast<dasi::Key*>(p_key);
+    tryCatch(error, [key, keyword, value] {
+        ASSERT(key != nullptr);
+        ASSERT(keyword != nullptr);
+        ASSERT(value != nullptr);
+        key->set(keyword, value);
+    });
+}
+
+void dasi_key_erase(dasi_key_t p_key, const char* keyword,
+                    dasi_error_t* error) {
+    auto* key = reinterpret_cast<dasi::Key*>(p_key);
+    tryCatch(error, [key, keyword] {
+        ASSERT(key != nullptr);
+        ASSERT(keyword != nullptr);
+        key->erase(keyword);
+    });
+}
+
+// -----------------------------------------------------------------------------
+//                           QUERY
+// -----------------------------------------------------------------------------
+
+dasi_query_t dasi_query_new(dasi_error_t* error) {
+    dasi::Query* result = nullptr;
+    tryCatch(error,
+             [&result] { result = std::make_unique<dasi::Query>().release(); });
+    return reinterpret_cast<dasi_query_t>(result);
+}
+
+void dasi_query_delete(dasi_query_t p_query, dasi_error_t* error) {
+    auto* query = reinterpret_cast<dasi::Query*>(p_query);
+    tryCatch(error, [query] {
+        ASSERT(query != nullptr);
+        delete query;
+    });
+}
+
+void dasi_query_set(dasi_query_t p_query, const char* keyword,
+                    const char* values[], size_t num, dasi_error_t* error) {
+    auto* query = reinterpret_cast<dasi::Query*>(p_query);
+    tryCatch(error, [query, keyword, values, num] {
+        ASSERT(query != nullptr);
+        ASSERT(keyword != nullptr);
+        ASSERT(values != nullptr);
+        query->set(keyword, dasi::Query::value_type{values, values + num});
+    });
+}
+
+void dasi_query_append(dasi_query_t p_query, const char* keyword,
+                       const char* value, dasi_error_t* error) {
+    auto* query = reinterpret_cast<dasi::Query*>(p_query);
+    tryCatch(error, [query, keyword, value] {
+        ASSERT(query != nullptr);
+        ASSERT(keyword != nullptr);
+        ASSERT(value != nullptr);
+        query->append(keyword, value);
+    });
+}
---
+++ src/dasi/api/c/Dasi.h
@@ -0,0 +1,137 @@
+/*
+ * (C) Copyright 2023- ECMWF.
+ *
+ * This software is licensed under the terms of the Apache Licence Version 2.0
+ * which can be obtained at http: //www.apache.org/licenses/LICENSE-2.0.
+ * In applying this licence, ECMWF does not waive the privileges and immunities
+ * granted to it by virtue of its status as an intergovernmental organisation
+ * nor does it submit to any jurisdiction.
+ */
+
+/**
+ * @file DASI C API Conventions
+ *
+ * - #define and Enum constants should be all CAPS.
+ * - Function, variable, typedef, struct, enum tag names should be lower case.
+ *   Separate the words by underscore "_" for clarity. Typedefs end with "_t".
+ * - Functions that are PUBLIC should start with "dasi_".
+ * - Each object is represented as a pointer to an opaque structure.
+ * - If the function creates a new object, it has to be released using the
+ *   appropriate "<obj>_delete" call.
+ *
+ */
+
+#pragma once
+
+#include <stddef.h>
+
+// -----------------------------------------------------------------------------
+//                           TYPES
+// -----------------------------------------------------------------------------
+
+typedef struct Dasi* dasi_t;
+typedef struct Key* dasi_key_t;
+typedef struct Query* dasi_query_t;
+typedef struct DasiError* dasi_error_t;
+
+/// DASI Error Codes
+typedef enum dasi_error_enum_t
+{
+    DASI_SUCCESS        = 0, /* Operation succeded. */
+    DASI_ERROR          = 1, /* Operation failed. */
+    DASI_ERROR_UNKNOWN  = 2, /* Failed with an unknown error. */
+    DASI_ERROR_USER     = 3, /* Failed with an user error. */
+    DASI_ERROR_ITERATOR = 4  /* Failed with an iterator error. */
+} dasi_error_enum_t;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// -----------------------------------------------------------------------------
+//                           ERROR HANDLING
+// -----------------------------------------------------------------------------
+
+/**
+ * @brief Get the error message from the error object.
+ * @param error Error object.
+ * @return const char* Error message.
+ */
+const char* dasi_error_get_message(const dasi_error_t error);
+
+/**
+ * @brief Get the error code from the error object.
+ * @param error Error object.
+ * @return dasi_error_enum_t Error code.
+ */
+dasi_error_enum_t dasi_error_get_code(const dasi_error_t error);
+
+// -----------------------------------------------------------------------------
+//                           SESSION
+// -----------------------------------------------------------------------------
+
+/// Create a new session object using the given configuration file.
+dasi_t dasi_new(const char* filename, dasi_error_t* error);
+
+/// Release the session and delete the object.
+void dasi_delete(dasi_t p_session, dasi_error_t* error);
+
+/**
+ * @brief Write data to the object
+ *
+ * @note Data is not guaranteed accessible nor persisted (wrt. failure),
+ * until dasi_flush() is called.
+ *
+ * @param p_session Dasi session
+ * @param p_key Metadata description of the data to store and index
+ * @param data Pointer to the read-only data
+ * @param length Length of "data" in bytes
+ * @param error Error object
+ */
+void dasi_archive(dasi_t p_session, dasi_key_t p_key, const void* data,
+                  size_t length, dasi_error_t* error);
+
+/// Flushes all buffers and ensures the internal state is safe.
+void dasi_flush(dasi_t p_session, dasi_error_t* error);
+
+// -----------------------------------------------------------------------------
+//                           KEY
+// -----------------------------------------------------------------------------
+
+/// Create a new key object.
+dasi_key_t dasi_key_new(dasi_error_t* error);
+
+/// Release the key and delete the object.
+void dasi_key_delete(dasi_key_t p_key, dasi_error_t* error);
+
+/// Set the value of the specified keyword.
+/// @note The keyword is added if it's missing.
+void dasi_key_set(dasi_key_t p_key, const char* keyword, const char* value,
+                  dasi_error_t* error);
+
+/// Erase the keyword:value pair specified by its keyword.
+void dasi_key_erase(dasi_key_t p_key, const char* keyword, dasi_error_t* error);
+
+// -----------------------------------------------------------------------------
+//                           QUERY
+// -----------------------------------------------------------------------------
+
+/// Create a new query object.
+dasi_query_t dasi_query_new(dasi_error_t* error);
+
+/// Release a query object.
+void dasi_query_delete(dasi_query_t p_query, dasi_error_t* error);
+
+/// Set all values for a keyword at once.
+/// @note The keyword is added if it's missing.
+void dasi_query_set(dasi_query_t p_query, const char* keyword,
+                    const char* values[], size_t num, dasi_error_t* error);
+
+/// Append one value to the set for the given keyword.
+/// @note The keyword is added if it's missing.
+void dasi_query_append(dasi_query_t p_query, const char* keyword,
+                       const char* value, dasi_error_t* error);
+
+#ifdef __cplusplus
+}
+#endif
