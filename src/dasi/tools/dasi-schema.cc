99074ccfc282b4366f8bc064b2fdf00aba2da5fd
Metin Cakircali <metin.cakircali@ecmwf.int>
2023-08-08 16:54:13 +0200

    feat(dasi-schema): added --list option

--- src/dasi/tools/dasi-schema.cc
+++ src/dasi/tools/dasi-schema.cc
@@ -24,6 +24,7 @@
 #include "dasi/api/Dasi.h"
 
 #include <regex>
+#include <filesystem>
 
 using namespace eckit::option;
 
@@ -38,100 +39,159 @@ public:  // methods
             new SimpleOption<bool>("query", "Prints out the query string(s) (defined in the schema file)."));
         options_.push_back(new SimpleOption<bool>(
             "scan", "Prints out the keys found in the root paths (defined in the configuration yaml file)."));
+        options_.push_back(
+            new SimpleOption<bool>("root", "Prints out the root paths defined in the configuration yaml file."));
+        options_.push_back(new SimpleOption<bool>("list", "Prints out the data and index paths for a given query."));
     }
 
 private:  // methods
     void usage(const std::string& tool) const override;
 
-    void init(const eckit::option::CmdArgs& args) override;
+    void init(const CmdArgs& args) override;
 
-    void execute(const eckit::option::CmdArgs& args) override;
+    void execute(const CmdArgs& args) override;
 
-    std::vector<std::vector<std::string>> tokenizeRules();
+    using RuleList = std::vector<std::vector<std::string>>;
+
+    [[nodiscard]]
+    RuleList tokenizeRules();
+
+    using PathList = std::vector<eckit::LocalPathName>;
+
+    [[nodiscard]]
+    PathList getRootPaths() const;
+
+    void query();
 
     void scan();
+
+    void list(const CmdArgs& args);
 
 private:  // members
     bool query_ {false};
     bool scan_ {false};
 
-    std::vector<std::vector<std::string>> rules_;
+    RuleList rules_;
 };
 
 //----------------------------------------------------------------------------------------------------------------------
 
 void DASISchema::usage(const std::string& tool) const {
-    eckit::Log::info() << std::endl
-                       << "Usage: " << tool << " [options]" << std::endl
-                       << std::endl
-                       << std::endl
-                       << "Examples:" << std::endl
-                       << "=========" << std::endl
-                       << std::endl
-                       << tool << " --config=dasi.yaml" << std::endl
-                       << tool << " --query" << std::endl
-                       << tool << " --scan" << std::endl
-                       << std::endl;
+    eckit::Log::info() << eckit::newl                                       //
+                       << "Usage: " << tool << " [options]" << eckit::newl  //
+                       << eckit::newl                                       //
+                       << eckit::newl                                       //
+                       << "Examples:" << eckit::newl                        //
+                       << "=========" << eckit::newl                        //
+                       << eckit::newl                                       //
+                       << tool << " --config=dasi.yaml" << eckit::newl      //
+                       << tool << " --query" << eckit::newl                 //
+                       << tool << " --scan" << eckit::newl                  //
+                       << tool << " --root" << eckit::newl                  //
+                       << tool << " --list key1=value1,key3=value3" << eckit::newl;
     DASITool::usage(tool);
 }
 
-void DASISchema::init(const eckit::option::CmdArgs& args) {
+void DASISchema::init(const CmdArgs& args) {
     query_ = args.getBool("query", query_);
     scan_  = args.getBool("scan", scan_);
-    if (query_ || scan_) {
-        rules_ = tokenizeRules();
-    } else {
-        eckit::Log::info() << "No option is provided!" << std::endl;
-        usage(name());
-    }
+
+    if (query_ || scan_) { rules_ = tokenizeRules(); }
 }
 
-void DASISchema::execute(const eckit::option::CmdArgs& /* args */) {
-    // print out the rules in schema
-    if (query_) {
-        std::ostringstream oss;
-        for (auto&& rule : rules_) {
-            oss << "{";
-            for (auto&& key : rule) {
-                const char* separator = ",";
-                if (&key == &rule.back()) { separator = ""; }
-                oss << key << "=%s" << separator;
-            }
-            oss << "}" << std::endl;
-        }
-        // print out to std
-        std::cout << oss.str();
-    }
+void DASISchema::execute(const CmdArgs& args) {
+    if (query_) { return query(); }
 
-    if (scan_) { scan(); }
+    if (scan_) { return scan(); }
+
+    if (args.getBool("list", false)) { return list(args); }
+
+    if (args.getBool("root", false)) {
+        for (auto&& root : getRootPaths()) { eckit::Log::info() << root << eckit::newl; }
+    }
 }
 
 //----------------------------------------------------------------------------------------------------------------------
 
-void DASISchema::scan() {
-    using PathList = std::vector<eckit::LocalPathName>;
+DASISchema::RuleList DASISchema::tokenizeRules() {
+    std::vector<std::string> rules;
 
+    {
+        std::stringstream sbuf;
+        dasi().dumpSchema(sbuf);
+
+        std::string line;
+        while (std::getline(sbuf, line)) { rules.push_back(line); }
+    }
+
+    RuleList result;
+
+    for (auto&& rule : rules) {
+        // format
+        rule.erase(0, 1);
+        std::replace(rule.begin(), rule.end(), '[', ',');
+        rule = std::regex_replace(rule, std::regex("[ \\]\\?]"), "");
+
+        // tokenize (comma delimited)
+        std::vector<std::string> tokens;
+        {
+            std::istringstream sbuf(rule);
+            std::string        token;
+            while (std::getline(sbuf, token, ',')) { tokens.push_back(token); }
+        }
+
+        result.push_back(tokens);
+    }
+
+    return result;
+}
+
+//----------------------------------------------------------------------------------------------------------------------
+
+DASISchema::PathList DASISchema::getRootPaths() const {
     // find the root paths defined in the config yaml
     const auto config = eckit::YAMLParser::decodeFile(configPath_);
     const auto roots  = config["spaces"]["handler"]["roots"];
     ASSERT(roots.isList());
 
-    for (size_t i = 0; i < roots.size(); ++i) {
+    PathList result;
+    for (size_t i = 0; i < roots.size(); ++i) { result.emplace_back(roots[i]["path"]); }
+    return result;
+}
+
+//----------------------------------------------------------------------------------------------------------------------
+
+void DASISchema::query() {
+    for (auto&& rule : rules_) {
+        eckit::Log::info() << "{";
+        for (auto&& key : rule) {
+            const char* separator = ",";
+            if (&key == &rule.back()) { separator = ""; }
+            eckit::Log::info() << key << "=%s" << separator;
+        }
+        eckit::Log::info() << "}" << eckit::newl;
+    }
+}
+
+//----------------------------------------------------------------------------------------------------------------------
+
+void DASISchema::scan() {
+    for (auto&& path : getRootPaths()) {
+        // directories found in this root path
         PathList dirs;
 
         {
-            PathList   files;
-            const auto path = roots[i]["path"];
-            eckit::LocalPathName(path).children(files, dirs);
+            PathList files;
+            path.children(files, dirs);
         }
 
         for (auto&& dir : dirs) {
             std::string value;
             {
-                // get the first value from the folder name
-                std::stringstream ss;
-                ss << (dir.baseName(false));
-                std::getline(ss, value, ':');
+                // get the first value from the directory name
+                std::stringstream sbuf;
+                sbuf << (dir.baseName(false));
+                std::getline(sbuf, value, ':');
             }
             for (auto&& rule : rules_) {
                 dasi::Query query(rule[0] + "=" + value);
@@ -144,37 +204,20 @@ void DASISchema::scan() {
     }
 }
 
-std::vector<std::vector<std::string>> DASISchema::tokenizeRules() {
-    std::vector<std::string> ruleLines;
+//----------------------------------------------------------------------------------------------------------------------
 
-    {
-        std::stringstream ss;
-        dasi().dumpSchema(ss);
+void DASISchema::list(const CmdArgs& args) {
+    for (size_t i = 0; i < args.count(); ++i) {
+        dasi::Query q(args(i));
+        for (const auto& elem : dasi().list(q)) {
+            std::filesystem::path fpath = elem.location.uri.name();
 
-        std::string line;
-        while (std::getline(ss, line)) { ruleLines.push_back(line); }
-    }
-
-    std::vector<std::vector<std::string>> ruleList;
-
-    for (auto&& rule : ruleLines) {
-        // format
-        rule.erase(0, 1);
-        std::replace(rule.begin(), rule.end(), '[', ',');
-        rule = std::regex_replace(rule, std::regex("[ \\]\\?]"), "");
-
-        // tokenize (comma delimited)
-        std::vector<std::string> tokens;
-        {
-            std::istringstream iss(rule);
-            std::string        token;
-            while (std::getline(iss, token, ',')) { tokens.push_back(token); }
+            if (fpath.extension() == ".data") {
+                eckit::Log::info() << fpath << eckit::newl;                              // file.data
+                eckit::Log::info() << fpath.replace_extension(".index") << eckit::newl;  // file.index
+            }
         }
-
-        ruleList.push_back(tokens);
     }
-
-    return ruleList;
 }
 
 }  // namespace dasi::tools
